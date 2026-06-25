4d4ca4e543163271de20753e1ed4bf25e9a79801
mstfsr888-web <mstfsr888@gmail.com>
2026-06-25 17:02:14 +0200

    Update connectors.module.ts

--- src/connectors/connectors.module.ts
+++ src/connectors/connectors.module.ts
@@ -1,7 +1,1 @@
-import { Module } from '@nestjs/common';
-import { ConnectorsController } from './connectors.controller';
-import { GmailService } from './gmail.service';
-import { AiModule } from '../ai/ai.module';
 
-@Module({ imports: [AiModule], controllers: [ConnectorsController], providers: [GmailService] })
-export class ConnectorsModule {}
