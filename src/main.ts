f2d184289cfcefe7723057b184e576d00df2eba2
mstfsr888-web <mstfsr888@gmail.com>
2026-06-27 23:22:17 +0200

    Add files via upload

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,23 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('[boot] 1/4 creating Nest application...');
   const app = await NestFactory.create(AppModule);
+
+  console.log('[boot] 2/4 app created, applying config...');
   app.setGlobalPrefix('api/v1');
   app.enableCors({ origin: process.env.WEB_ORIGIN || '*' });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   const port = Number(process.env.PORT || 3000);
-  await app.listen(port);
-  // eslint-disable-next-line no-console
-  console.log(`FamPilot API on http://localhost:${port}/api/v1`);
+  console.log(`[boot] 3/4 calling listen on 0.0.0.0:${port} ...`);
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`[boot] 4/4 FamPilot API UP on :${port}/api/v1`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  // eslint-disable-next-line no-console
+  console.error('[boot] FATAL during bootstrap:', err);
+  process.exit(1);
+});
